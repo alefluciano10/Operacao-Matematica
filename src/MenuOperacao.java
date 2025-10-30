cb062d4fe2bce2276b703b35dc876086b3e56953
alefluciano10 <alef.luciano@gmail.com>
2025-10-30 12:46:26 -0300

    Correção do menu para não aceitar letras

--- src/MenuOperacao.java
+++ src/MenuOperacao.java
@@ -6,23 +6,25 @@ public class MenuOperacao {
     private final Scanner ler = new Scanner(System.in);
 
     public int exibirMenuOperacao() {
-        System.out.println("\n===============================");
-        System.out.println("      🧮 CALCULADORA JAVA      ");
-        System.out.println("===============================");
-        System.out.println("1 ➤ Adição");
-        System.out.println("2 ➤ Subtração");
-        System.out.println("3 ➤ Multiplicação");
-        System.out.println("4 ➤ Divisão");
-        System.out.println("5 ➤ Histórico");
-        System.out.println("0 ➤ Sair");
-        System.out.print("\nEscolha uma operação: ");
+        while (true) {
+            System.out.println("\n===============================");
+            System.out.println("      🧮 CALCULADORA JAVA      ");
+            System.out.println("===============================");
+            System.out.println("1 ➤ Adição");
+            System.out.println("2 ➤ Subtração");
+            System.out.println("3 ➤ Multiplicação");
+            System.out.println("4 ➤ Divisão");
+            System.out.println("5 ➤ Histórico");
+            System.out.println("0 ➤ Sair");
+            System.out.print("\nEscolha uma operação: ");
 
-        try {
-            return ler.nextInt();
-        } catch (InputMismatchException e) {
-            System.out.println("\n❌ Entrada inválida! Digite apenas números.");
-            ler.next(); // limpa o buffer
-            return -1;
+            String input = ler.nextLine(); // Lê como String
+            try {
+                int operacao = Integer.parseInt(input); // Converte para inteiro
+                return  operacao;
+            } catch (NumberFormatException e) {
+                System.out.println("\n❌ Entrada inválida! Digite apenas números.");
+            }
         }
     }
 }
